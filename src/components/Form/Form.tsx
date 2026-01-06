aa79599d2b2fb5330f7576694d9d3d85639805a1
Yosber Guerrero <yosbergc@gmail.com>
2026-01-06 14:58:51 -0400

    Added select input

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,34 +1,52 @@
+import { Select } from "@/components/base/select/select";
+import { SelectItem } from "../base/select/select-item";
 function Form() {
+    const items = [
+        { label: 'Frontend Developer', id: 'frontend'},
+        { label: 'Backend Developer', id: 'backend'},
+        { label: 'Fullstack Developer', id: 'fullstack'},
+        { label: 'Mobile Developer', id: 'mobile'},
+        { label: 'Experto en Ciberseguridad', id: 'ciberseguridad'},
+        { label: 'Cloud Engineer / DevOps', id: 'devops'},
+        { label: 'Data Scientist / Analista de Datos', id: 'analista'},
+        { label: 'QA Tester', id: 'qa'},
+        { label: 'Growth Hacker', id: 'growth'},
+        { label: 'SEO Specialist', id: 'seo'},
+        { label: 'Trafficker Digital / Media Buyer', id: 'media buyer'},
+        { label: 'Community Manager', id: 'community manager'},
+        { label: 'Copywriter', id: 'copywriter'},
+        { label: 'E-commerce Manager', id: 'ecommerce manager'},
+        { label: 'Diseñador UX/UI', id: 'designer ux ui'},
+        { label: 'Diseñador Gráfico Digital', id: 'graphic designer'},
+        { label: 'Editor de Video / Motion Graphics', id: 'editor de video'},
+        { label: 'Animador 3D', id: 'animador 3d'},
+        { label: 'Ilustrador Digital', id: 'ilustrador'},
+        { label: 'Asistente Virtual', id: 'asistente'},
+        { label: 'Project Manager', id: 'project manager'},
+        { label: 'Customer Success / Soporte', id: 'soporte'},
+        { label: 'IT Recruiter', id: 'it recruiter'},
+    ]
     return (
         <section className="flex flex-col w-full max-w-4xl bg-white p-4 rounded-xl border-2 border-gray-300 max-w-xl">
             <h2 className="text-center font-extrabold text-slate-800 text-xl">Llena el formulario y optén tu CV optimizado</h2>
-            <select name="job" id="job" className="h-10 border-1 rounded-full px-4 my-4 font-medium" >
-                <option value="">Elige tu profesión</option>
-                <option value="">Frontend Developer</option>
-                <option value="">Backend Developer</option>
-                <option value="">Fullstack Developer</option>
-                <option value="">Mobile Developer</option>
-                <option value="">Experto en Ciberseguridad</option>
-                <option value="">Cloud Engineer / DevOps</option>
-                <option value="">Analista de Datos</option>
-                <option value="">QA Tester</option>
-                
-                <option value="">Backend Developer</option>
-                <option value="">Backend Developer</option>
-                <option value="">Backend Developer</option>
-                <option value="">Backend Developer</option>
-                <option value="">Backend Developer</option>
-                <option value="">Backend Developer</option>
-                <option value="">Backend Developer</option>
-                <option value="">Backend Developer</option>
+            <Select
+                isRequired
+                label="Tu profesión"
+                placeholder="Selecciona tu profesión"
+                tooltip="Elije la profesión que consideres que más se acerca a tu día a día o al trabajo que estás aplicando."
+                items={items}
+                className="mt-4"
+                >
+                {items.map((item) => (
+                    <SelectItem
+                    key={item.id} 
+                    id={item.id} 
 
-            </select>
-            <section>
-                <textarea name="jobDescription" id="jobDescription" className="bg-gray-200 rounded-md border-2 border-gray-400 w-full inline-block p-4" rows={8}/>
-            </section>
-            <section>
-                <label htmlFor="" className="text-sm text-gray-800"></label>
-            </section>
+                    >
+                    {item.label}
+                    </SelectItem>
+                ))}
+            </Select>
         </section>
     )
 }
